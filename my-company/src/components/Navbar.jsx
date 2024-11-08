b679991e0129527a932b42d124fd6c78c097a11d
carolyne wanjiru <carolynekamau097@gmail.com>
2024-11-08 20:13:42 +0300

    My company application

---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav style={{ padding: '10px', borderBottom: '1px solid #ccc' }}>
+      <Link to="/" style={{ margin: '0 10px' }}>Home</Link>
+      <Link to="/about" style={{ margin: '0 10px' }}>About</Link>
+      <Link to="/services" style={{ margin: '0 10px' }}>Services</Link>
+      <Link to="/contact" style={{ margin: '0 10px' }}>Contact</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
