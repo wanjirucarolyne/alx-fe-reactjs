ea72a6014eee5fe53ee4075282e549fbfed29a16
carolyne wanjiru <carolynekamau097@gmail.com>
2024-11-08 20:24:36 +0300

    My company application

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,7 +3,13 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ padding: '10px', borderBottom: '1px solid #ccc' }}>
+    <nav 
+        style={{
+            backgroundColor: '#333',
+            display: 'flex',
+            justifyContent: 'space-around',
+            padding: '10px'
+         }}>
       <Link to="/" style={{ margin: '0 10px' }}>Home</Link>
       <Link to="/about" style={{ margin: '0 10px' }}>About</Link>
       <Link to="/services" style={{ margin: '0 10px' }}>Services</Link>
