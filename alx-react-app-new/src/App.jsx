ed3cd3a81b373be870690bdf98eb0302002b6bdf
carolyne wanjiru <carolynekamau097@gmail.com>
2024-11-08 14:45:54 +0300

    New use of Usestate

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,12 +7,14 @@ import Header from './components/Header'
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Counter />
     <WelcomeMessage />
     <Header />
     <MainContent /> 
