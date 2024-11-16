a0f23e77dacd7115ed83eb38a9e0ba26c28a9afa
carolyne wanjiru <carolynekamau097@gmail.com>
2024-11-16 14:20:35 +0300

    Initial commit

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,15 @@
+
+import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
+function App() {
+  return (
+    <div className="App">
+      <h1>Recipe Sharing App</h1>
+      <AddRecipeForm />
+      <RecipeList />
+      
+    </div>
+  );
+}
+
+export default App;
