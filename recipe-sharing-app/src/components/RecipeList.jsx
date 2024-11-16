74b82cb41fcdd088cf1865ca8dac4f543ba82f07
carolyne wanjiru <carolynekamau097@gmail.com>
2024-11-16 18:00:53 +0300

    search and filtering capabilities added

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,4 +1,5 @@
 import  useRecipeStore  from './recipeStore';
+import { Link } from 'react-router-dom';
 
   const RecipeList = () => {
     const recipes = useRecipeStore(state => state.recipes);
@@ -11,7 +12,9 @@ import  useRecipeStore  from './recipeStore';
       <div>
         {recipes.map(recipe => (
           <div key={recipe.id}>
-            <h3>{recipe.title}</h3>
+            <Link to={`/recipe/${recipe.id}`}>
+              <h3>{recipe.title}</h3>
+            </Link>
             <p>{recipe.description}</p>
           </div>
         ))}
