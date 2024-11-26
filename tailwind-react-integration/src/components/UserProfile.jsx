719f2d513611e9902262a9af744cfe3e103bbfb3
carolyne wanjiru <carolynekamau097@gmail.com>
2024-11-26 15:53:08 +0300

    Responsiveness added

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,13 +1,13 @@
 function UserProfile() {
     return (
-      <div className="bg-gray-100 p-4 sm:p-8 max-w-xs sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+      <div className="bg-gray-100 sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
         <img 
           src="https://via.placeholder.com/150" 
           alt="User" 
-          className="rounded-full w-24 h-24 sm:w-36 sm:h-36 mx-auto"
+          className="rounded-full sm:w-24  md:w-36 h-36 mx-auto"
         />
-        <h1 className="text-lg sm:text-xl text-blue-800 my-4">John Doe</h1>
-        <p className="text-sm sm:text-base text-gray-600 ">
+        <h1 className="sm:text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
+        <p className="sm:text-sm md:text-base text-gray-600 ">
           Developer at Example Co. Loves to write code and explore new technologies.
         </p>
       </div>
